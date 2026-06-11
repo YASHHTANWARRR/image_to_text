d704184291f2188575e79ad0efd65aceb846ab13
YASHHTANWARRR <yashtanwar11092005@gmail.com>
2026-06-11 15:32:06 +0530

    loaded my files

--- src/dataset.py
+++ src/dataset.py
@@ -5,43 +5,45 @@ import PIL
 import torch
 from torch.utils.data import dataset
 
-def _init_(self,img_dir,csv_file,tokenizer,feature_extractor,
-            maxlength=64):
-    self.df = pd.read_csv(csv_file)
+class FlickrDataset(dataset):
     
-    self.image_dir = img_dir 
-    
-    self.tokenizer = tokenizer
-    
-    self.feature_extractor = feature_extractor
-    
-    self.max_length = maxlength
+    def _init_(self,img_dir,csv_file,tokenizer,feature_extractor,
+                maxlength=64):
+        self.df = pd.read_csv(csv_file)
+        
+        self.image_dir = img_dir 
+        
+        self.tokenizer = tokenizer
+        
+        self.feature_extractor = feature_extractor
+        
+        self.max_length = maxlength
 
-def _len_(self):
-    return len(self,df)
+    def _len_(self):
+        return len(self,df)
 
-def _get_(self,idx):
-    image_path= (
-        f"{self.img_dir}/"
-        f"{self.df.iloc[idx]["image"]}"
-    )
-    caption = self.df.iloc[idx][caption]
-    
-    image = Image.open(image_path).conver("RGB")
-    
-    pixel_values= feature_extractor(images=image,
-                                    return_tensors="pt").pixel_values.squeeze(0)
-    
-    labels = self.tokenizer(
-        caption,
-        padding="max_Length",
-        truncation= True,
-        max_Length = self.max_length,
-        return_tensors="pt"
-    ).imput_ids.squeeze(0)
-    
-    return {
-        "labels": labels,
-        "pixel_values": pixel_values
-    }
+    def _get_(self,idx):
+        image_path= (
+            f"{self.img_dir}/"
+            f"{self.df.iloc[idx]["image"]}"
+        )
+        caption = self.df.iloc[idx][caption]
+        
+        image = Image.open(image_path).conver("RGB")
+        
+        pixel_values= feature_extractor(images=image,
+                                        return_tensors="pt").pixel_values.squeeze(0)
+        
+        labels = self.tokenizer(
+            caption,
+            padding="max_Length",
+            truncation= True,
+            max_Length = self.max_length,
+            return_tensors="pt"
+        ).imput_ids.squeeze(0)
+        
+        return {
+            "labels": labels,
+            "pixel_values": pixel_values
+        }
     
--- src/train.py
+++ src/train.py
@@ -14,6 +14,15 @@ batch_size = 16
 lr = 3e-4 
 
 def train():
-    model,feature_extractor,tokenizer
+    model, feature_extractor, tokenizer = load_model()
+    
+    model.to(device)
+    
+    dataset = FlickrDataset(
+                img_file='/home/hornet/dataset_folders/archive/Images/flickr30k_images',
+                csv_file='/home/hornet/dataset_folders/archive/context.txt',
+                tokenizer=tokenizer,
+                feature_extractor=feature_extractor
+    )
 
 
