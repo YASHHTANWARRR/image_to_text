9edb41f10bb72f134fe460e5f4201afab0dc9373
YASHHTANWARRR <yashtanwar11092005@gmail.com>
2026-06-11 15:18:06 +0530

    initialisation of functiosn little bit

--- src/dataset.py
+++ src/dataset.py
@@ -5,8 +5,34 @@ import PIL
 import torch
 from torch.utils.data import dataset
 
-def _init_():
-    self,
-    img_dir,
-    csv_file,
+def _init_(self,img_dir,csv_file,tokenizer,feature_extractor,
+            maxlength=64):
+    self.df = pd.read_csv(csv_file)
+    
+    self.image_dir = img_dir 
+    
+    self.tokenizer = tokenizer
+    
+    self.feature_extractor = feature_extractor
+    
+    self.max_length = maxlength
+
+def _len_(self):
+    return len(self,df)
+
+def _get_(self,idx):
+    image_path= (
+        f"{self.img_dir}/"
+        f"{self.df.iloc[idx]["image"]}"
+    )
+    caption = self.df.iloc[idx][caption]
+    
+    image = Image.open(image_path).conver("RGB")
+    
+    pixel_values= feature_extractor(images=image,
+                                    return_tensors="pt").pixel_values.squeeze(0)
+    
+    labels = self.tokenizer(
+        
+    )
     
